bcd837a6727896b1c690a7a992d62c692e5c0e30
pankaj suthar <pankajss0070@gmail.com>
2024-11-11 18:48:31 +0530

    add kanba

--- app/home/workspaces/[id]/page.tsx
+++ app/home/workspaces/[id]/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { KanbanBoard } from "@/components/kanban/kanban-board";
+import NewTaskDialog from "@/components/kanban/new-task-dialog";
 import { Ellipsis } from "lucide-react";
 import { useParams } from "next/navigation";
 import React from "react";
@@ -14,8 +16,11 @@ const Workspace = () => {
         <p className="font-bold text-white text-xl">My Board</p>
         <Ellipsis color="white" className="cursor-pointer" />
       </div>
-      <div className="p-4">
-        <div className=" bg-red-300 h-[200px] w-[200px]"></div>
+      <div className="px-4 overflow-auto h-[calc(100vh-127.6px)] w-screen">
+        <div className="flex items-start justify-between mb-2">
+          <NewTaskDialog />
+        </div>
+        <KanbanBoard />
       </div>
     </div>
   );
